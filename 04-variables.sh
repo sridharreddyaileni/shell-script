9c36be4d45b48d4afb2df8c073182a033a62e3b5
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-07 20:26:25 +0530

    added date variable

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2, Good Morning"
+echo "$PERSON2: Hi $PERSON1 Very Good Morning"
+echo "$PERSON1: How are you doing $PERSON2?"
+echo "$PERSON2: Iam doing great $PERSON1, Thanks for asking and How about you?"
