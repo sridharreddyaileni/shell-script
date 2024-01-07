001a337c8d561b5f1baf866ea845c9096b6ef297
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-07 21:29:03 +0530

    added date variable

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,6 +4,10 @@ echo "Please enter your username"
 
 read -s USERNAME
 
-echo "Username is: $USERNAME"
+echo "Please enter your password"
+
+read -s PASSWORD
+
+echo "Username is: $USERNAME", Password is: $PASSWORD
 
 
