dd0b94c430ea4759c69fc00e425cdf77567228cd
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-07 21:55:06 +0530

    added date variable

---
+++ 07-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Banana" "Mango")
+
+echo "First value: ${FRUITS[0]}"
+
+echo "Second value: ${FRUITS[2]}"
+
+echo "Third value: ${FRUITS[1]}"
+
+echo "Fourth value: ${FRUITS[3]}"
