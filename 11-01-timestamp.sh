14e65ced2e864a5c541528c00fd8bde46ca8d72f
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-08 17:45:23 +0530

    script updated

---
+++ 11-01-timestamp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script name: $0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR:: $2....failed"
+        exit 1
+    else
+        echo "$2....success"
+    fi
+}
+
+if [ $ID -ne 0 ]  
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+yum install mysql -y
+
+VALIDATE $? "installing mysql"
