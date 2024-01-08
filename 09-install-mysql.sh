379cf138fc78c6b483825d93c2b6f04ff6967250
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-08 10:49:24 +0530

    script upated

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-#ID=$(id -u)
+ID=$(id -u)
 
 VALIDATE(){
     if [ $? ne 0 ]
     then 
         echo "Installation is Failed"
-       #exit 1
+        exit 1
     else
         echo "Installation is Success"
     fi
 }
 
-# if [ $ID -ne 0 ]
-# then
-#     echo "ERROR:: Please run this scripts with root access"
-#     #exit 1
-# else   
-#     echo "You are root user"
-# fi 
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this scripts with root access"
+    exit 1
+else   
+    echo "You are root user"
+fi 
 
-yum install mysqql -y
+yum install mysql -y
 
 VALIDATE
 
+yum install git 
+
+VALIDATE
 
 
