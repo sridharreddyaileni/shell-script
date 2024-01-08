ba3d25f89b1a6f505d5f2310d90ab45893aa8516
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-08 11:50:37 +0530

    script upated

---
+++ 09-01-exitstatus.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this scripts with root access"
+    exit 1
+else   
+    echo "You are root user"
+fi 
+
+yum install mysql -y
+
+if [ $? ne 0 ]
+then
+    echo "ERROR:: Installation of mysql failed"
+    exit 1
+else
+    echo "Instllation mysqlis success"
+fi 
