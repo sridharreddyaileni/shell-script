508ad933c5e0d79507a94909e6b9d4acb2c244c5
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-08 19:42:30 +0530

    script updated

--- 11-01-timestamp.sh
+++ 11-01-timestamp.sh
@@ -9,10 +9,10 @@ echo "script name: $0"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "ERROR:: $2....failed"
+        echo -e "ERROR:: $2....\e[31m failed \e[0m"
         exit 1
     else
-        echo -e "$2....\e[32m success \e[0m"
+        echo -e "$2....\e[32m success \e[0m" #colour code given for green and normal colour
     fi
 }
 
@@ -24,10 +24,10 @@ else
     echo "you are root user"
 fi
 
-yum install mysql -y &>> $LOGFILE
+yum install mysqll -y &>> $LOGFILE
 
 VALIDATE $? "installing mysql"
 
-yum install git -y &>> $LOGFILE
+yum install gitt -y &>> $LOGFILE
 
 VALIDATE $? "installing git"
