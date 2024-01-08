d5a8e3b753869e3897476cd3f98505211f4ad16a
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-08 12:51:04 +0530

    script upated

--- 09-01-exitstatus.sh
+++ 09-01-exitstatus.sh
@@ -2,18 +2,6 @@
 
 ID=$(id -u)
 
-X=$(echo $?)
-
-VALIDATE(){
-if [ $X -ne 0 ]
-then
-    echo "ERROR:: Installation of git failed"
-    exit 1
-else
-    echo "Instllation git is success"
-fi 
-}
-
 if [ $ID -ne 0 ]
 then
     echo "ERROR:: Please run this scripts with root access"
@@ -22,8 +10,15 @@ else
     echo "You are root user"
 fi 
 
-yum install ngixyy -y
+yum install mysqll -y
 
-VALIDATE
+if [ $? ne 0 ]
+then
+    echo "ERROR:: Installation of mysql failed"
+    exit 1
+else
+    echo "Instllation mysql is success"
+fi 
 
-#if [ $? ne 0 ]
+
+
