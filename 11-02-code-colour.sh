c1041ae7c4e9679ed21ac5385c1c2ed68b04436e
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-08 20:09:54 +0530

    script updated

--- 11-02-code-colour.sh
+++ 11-02-code-colour.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-TIMESTAMP=$(date +%F-%H-%M-%S)
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGFILE="/tmp/$0-$TIMESTAMP.log"
-
 echo -e "$G Hello $N"
 echo -e "$R I'M  ANGRY ON  YOU $N"
-echo -e "$Y just kidding $N" &>> $LOGFILE
-
-
+echo -e "$Y just kidding $N"
