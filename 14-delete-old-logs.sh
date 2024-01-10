c56c2e66ce65b35b762ac7846246a3c021a0b1e0
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-10 22:59:22 +0530

    script udpated

--- 14-delete-old-logs.sh
+++ 14-delete-old-logs.sh
@@ -8,16 +8,17 @@ Y="\e[33m"
 N="\e[0m"
 
 if [ ! -d $SOURCE_DIR ]
+
 then
-    echo -e "$R source directory: $SOURCE_DIR does not exists. $N"
+    echo -e "$R source directory: $SOURCE_DIR does not exists $N"
 fi
 
 FILES_TO_DELETE=$(find. -type f -mtime +14 -name "*.log") 
 
 while IFS= read -r line
+
 do 
-    echo "Deleting file: $line"
+
+    echo "Deleting file:" $line
+
 done <<< $FILES_TO_DELETE
-
-
-
